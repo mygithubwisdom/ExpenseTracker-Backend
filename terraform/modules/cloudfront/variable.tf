af7d2e184536711e4078cfa045a5c2dc541b1867
Trackamapp <contacttrackam25@gmail.com>
2025-08-16 17:01:14 +0100

    Delete terraform directory

--- terraform/modules/cloudfront/variable.tf
+++
@@ -1,3 +0,0 @@
-#variable "bucket_name" {}
-#variable "bucket_arn" {}
-#variable "bucket_domain" {}
--- terraform/modules/ses/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ses_email_identity" "femi_email" {
-  email = var.TRACKAM_EMAIL
-}
-
-resource "aws_ses_email_identity" "info_email" {
-  email = var.INFO_EMAIL
-}
-
